0891f7c004434c7b70c881cac7f5d3f3bddb7ee4
David Ng <dave@codeaurora.org>
2013-04-08 13:00:17 -0700

    Define initial APQ8084 target definition for bring-up
    Set up preliminary APQ8084 target definition.
    
    Change-Id: If545b2bb852f0a19570f0638d6c874ed73f1d0ea

---
+++ AndroidProducts.mk
@@ -0,0 +1,2 @@
+PRODUCT_MAKEFILES := \
+	$(LOCAL_DIR)/apq8084.mk
---
+++ apq8084.mk
@@ -0,0 +1,33 @@
+$(call inherit-product, device/qcom/common/common.mk)
+
+PRODUCT_NAME := apq8084
+PRODUCT_DEVICE := apq8084
+
+# Audio configuration file
+PRODUCT_COPY_FILES += \
+    device/qcom/apq8084/audio_policy.conf:system/etc/audio_policy.conf \
+
+# audio UCM files
+PRODUCT_COPY_FILES += device/qcom/apq8084/snd_soc_msm/snd_soc_msm_Taiko:system/etc/snd_soc_msm/snd_soc_msm_Taiko \
+                      device/qcom/apq8084/snd_soc_msm/snd_soc_msm_Taiko_CDP:system/etc/snd_soc_msm/snd_soc_msm_Taiko_CDP \
+                      device/qcom/apq8084/snd_soc_msm/snd_soc_msm_Taiko_Fluid:system/etc/snd_soc_msm/snd_soc_msm_Taiko_Fluid \
+                      device/qcom/apq8084/snd_soc_msm/snd_soc_msm_Taiko_liquid:system/etc/snd_soc_msm/snd_soc_msm_Taiko_liquid \
+
+
+# Feature definition files for apq8084
+PRODUCT_COPY_FILES += \
+    system/bluetooth/data/main.le.conf:system/etc/bluetooth/main.conf \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml
+
+#fstab.qcom
+PRODUCT_PACKAGES += fstab.qcom
+
+#OEM Services library
+PRODUCT_PACKAGES += oem-services
+PRODUCT_PACKAGES += libsubsystem_control
+PRODUCT_PACKAGES += libSubSystemShutdown
