4f84ee1560842c2f832c8041decfd37d43ad4b83
Ravit Dennis <rdennis@codeaurora.org>
2014-03-04 16:50:37 +0200

    apq8084: Add symbolic link to service settings
    Add symbolic link to service settings to be used
    by the digital pen system service
    
    Change-Id: If3ee99723f6c01bd15b08108d1b2023c5f5e27ae

--- usf_settings.sh
+++ usf_settings.sh
@@ -1,5 +1,5 @@
 #!/system/bin/sh
-# Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
+# Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
@@ -81,6 +81,8 @@ if [ ! -e $trigger_file ]; then
    ln -s $sg_dir/cfg/usf_sync_gesture_"$type".cfg $sg_dir/usf_sync_gesture.cfg
    ln -s $prox_dir/cfg/usf_proximity_"$type".cfg $prox_dir/usf_proximity.cfg
    ln -s $pairing_dir/cfg/usf_pairing_"$type".cfg $pairing_dir/usf_pairing.cfg
+
+   ln -s $e_dir/cfg/service_settings_"$type".xml $e_dir/service_settings.xml
 
    ln -s $mixer_dir/mixer_paths_"$type".xml $mixer_dir/mixer_paths.xml
 
