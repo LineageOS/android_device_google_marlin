tusharind007/sepolicy_vndr

sepolicy: Add policies for IPv6 Tethering
Added security policies needed for IPv6 tethering functionality
to perform operations on sockets

CRs-fixed: 628313
Change-Id: Ia5d88ecac78693aff672123492bf1cb3307110a8

Android.mk
@@ -8,4 +8,5 @@ BOARD_SEPOLICY_UNION := \
        file.te \
        device.te \
        vold.te \
-       file.te
+       file.te \
+       netd.te

LineageOS/android_device_google_marlin

sepolicy/Android.mk
@@ -18,4 +18,5 @@ BOARD_SEPOLICY_UNION := \
        smd_test.te \
        qmi_ping.te \
        qmi_test_service.te \
-       irsc_util.te
+       irsc_util.te \
+       netd.te