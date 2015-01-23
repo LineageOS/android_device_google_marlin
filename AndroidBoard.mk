94c61d8aded8cd010fbf9ee92e324096ddfe9eb1
Ameya Thakur <ameyat@codeaurora.org>
2015-01-23 11:23:09 -0800

    msm8996 : Add support for the msadp partition
    We now create a symlink from the system/etc/firmware directory
    to the msadp partition. This is needed in order for the
    request_firmware call to be able to locate the image.
    
    Change-Id: I1461197a453077e1fc14dc1c5ce72cbd139a42d3

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -128,8 +128,9 @@ include $(BUILD_PREBUILT)
 #Create symbolic links
 $(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wlan/qca_cld; \
         ln -sf /system/etc/wifi/WCNSS_qcom_cfg.ini \
-        $(TARGET_OUT_ETC)/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini)
-
+        $(TARGET_OUT_ETC)/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini; \
+        ln -sf /dev/block/bootdevice/by-name/msadp \
+        $(TARGET_OUT_ETC)/firmware/msadp )
 endif
 
 #----------------------------------------------------------------------
