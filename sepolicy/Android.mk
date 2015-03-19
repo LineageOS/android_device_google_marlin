c3ef5dde3ea75530aafc5f63fdeb593151efbfc9
Avijit Kanti Das <avijitnsec@codeaurora.org>
2015-03-19 12:05:13 -0700

    seandroid: Adding policy from other directories
    Adding policy from other directories maintaining the
    order of the policies.
    
    CRs-Fixed: 811138
    Change-Id: I0f34fa7ecb5ca90987b2b909e7caad6c768133e4

--- sepolicy/Android.mk
+++ sepolicy/Android.mk
@@ -1,12 +1,14 @@
 # Board specific SELinux policy variable definitions
 ifeq ($(call is-vendor-board-platform,QCOM),true)
 BOARD_SEPOLICY_DIRS := \
+       $(BOARD_SEPOLICY_DIRS) \
        device/qcom/sepolicy \
        device/qcom/sepolicy/common \
        device/qcom/sepolicy/test \
        device/qcom/sepolicy/$(TARGET_BOARD_PLATFORM)
 
 BOARD_SEPOLICY_UNION := \
+       $(BOARD_SEPOLICY_UNION) \
        genfs_contexts \
        file_contexts \
        service_contexts \
