052577d61207e62aaf7c59375ee53ab4af7094d1
Fenglin Wu <fenglinw@codeaurora.org>
2015-11-11 14:21:47 +0800

    msm8996: Start hvdcp_opti service in off-mode charging
    Hvdcp_opti service is need to run to support QC3.0 in off-mode charging.
    Regarding the binary source is located at /system/bin/ directory, mount
    system partition and also enable the dm-verity for the security.
    
    Change-Id: Iac33eb4e1f86deded999feb1bb7ec243dbb86137

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -89,6 +89,14 @@ LOCAL_SRC_FILES    := $(LOCAL_MODULE)
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := charger.fstab.qcom
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
 #include $(CLEAR_VARS)
 #LOCAL_MODULE       := init.qcom.modem_links.sh
 #LOCAL_MODULE_TAGS  := optional eng
