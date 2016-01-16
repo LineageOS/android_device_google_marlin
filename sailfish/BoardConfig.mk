14d49d2a5ee634b700838bc468411c1a26854873
xboxfanj <xboxlover360@gmail.com>
2016-01-16 18:45:48 +0100

    m1s1: Clean up kernel cmdline
     * CONFIG_DEBUG_USER is disabled, so no need to keep user_debug
       and the same goes for msm_rtb, which depends on CONFIG_MSM_RTB
    
    Change-Id: I9a30f32f7dc91cf82f2516bf6a54d7d62f1c00f1

--- sailfish/BoardConfig.mk
+++ sailfish/BoardConfig.mk
@@ -70,7 +70,7 @@ BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
 
 TARGET_USES_ION := true
 
-BOARD_KERNEL_CMDLINE += console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=sailfish user_debug=31 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff loop.max_part=7 androidboot.boot_devices=soc/624000.ufshc
+BOARD_KERNEL_CMDLINE += console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=sailfish ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff loop.max_part=7 androidboot.boot_devices=soc/624000.ufshc
 
 BOARD_ROOT_EXTRA_FOLDERS := firmware firmware/radio persist
 BOARD_ROOT_EXTRA_SYMLINKS := /vendor/lib/dsp:/dsp
