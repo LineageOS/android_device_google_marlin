9b0aa9d6d02c751a342818c472fdca624df3a02c
jonglin_lee <jonglin_lee@htc.com>
2016-05-06 18:27:24 +0800

    marlin: symlink bootdevice in recovery
    Bug: 28639949
    Bug: 28433543
    
    Change-Id: I17a351601408aefd0bcd5fc5ebde05fb55363fc2

--- device-sailfish.mk
+++ device-sailfish.mk
@@ -32,7 +32,8 @@ PRODUCT_COPY_FILES += \
     device/google/marlin/init.common.rc:root/init.sailfish.rc \
     device/google/marlin/init.common.usb.rc:root/init.sailfish.usb.rc \
     device/google/marlin/fstab.common:root/fstab.sailfish \
-    device/google/marlin/ueventd.common.rc:root/ueventd.sailfish.rc
+    device/google/marlin/ueventd.common.rc:root/ueventd.sailfish.rc \
+    device/google/marlin/init.recovery.common.rc:root/init.recovery.sailfish.rc
 
 # Audio tfa9891 firmware and config
 PRODUCT_COPY_FILES += \
