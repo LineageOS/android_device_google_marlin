7724e9fe7047423f75840328ffe1f58e82d766aa
Yueyao (Nathan) Zhu <yueyao@google.com>
2016-05-20 14:47:52 -0700

    marlin: move vendor modules to vendor partition
    Bug: 28054350
    Change-Id: I7fb89a77389043235f5352a0a54644acde263304

--- init.radio.sh
+++ init.radio.sh
@@ -3,8 +3,8 @@
 #
 # Copy qcril.db if needed for RIL
 #
-if [ -f /system/vendor/qcril.db -a ! -f /data/misc/radio/qcril.db ]; then
-    cp /system/vendor/qcril.db /data/misc/radio/qcril.db
+if [ -f /vendor/qcril.db -a ! -f /data/misc/radio/qcril.db ]; then
+    cp /vendor/qcril.db /data/misc/radio/qcril.db
     chown -h radio.radio /data/misc/radio/qcril.db
 fi
 echo 1 > /data/misc/radio/db_check_done
