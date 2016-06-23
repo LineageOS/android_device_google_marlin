e2d3721a5767b648c69a68321d3902b9e49a53d4
Dan Willemsen <dwillemsen@google.com>
2016-06-22 21:50:38 -0700

    Install .odex files to other system partition.
    Also enable copying them into data when appropriate.
    
    Bug 29278988
    
    Change-Id: I0a4cad2874cbd96f376059673865607f4ebec155

--- marlin/BoardConfig.mk
+++ marlin/BoardConfig.mk
@@ -179,6 +179,9 @@ TARGET_USE_VERIZON_APN_LIB_PREBUILT := true
 
 TARGET_BOARD_KERNEL_HEADERS := device/google/marlin/kernel-headers
 
+# Install odex files into the other system image
+BOARD_USES_SYSTEM_OTHER_ODEX := true
+
 -include vendor/google_devices/marlin/BoardConfigVendor.mk
 # Build a separate vendor.img
 TARGET_COPY_OUT_VENDOR := vendor
