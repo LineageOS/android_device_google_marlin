3e1330a15553cc4f147000bde784dd4545d9bd90
tim.tm_lin <tim.tm_lin@htc.com>
2016-07-28 15:01:23 +0800

    ril:limit the DSD indication during screen off
    for power saving during screen off
    not allow modem wake up AP by DSD system status indication
    bug:30437273
    Change-Id: I488a49adbd26047fe833b746db7d28c3b32b07d2
    Signed-off-by: tim.tm_lin <tim.tm_lin@htc.com>

--- device-common.mk
+++ device-common.mk
@@ -247,6 +247,7 @@ endif
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.cne.feature=1 \
+    persist.radio.data_ltd_sys_ind=1 \
     persist.radio.is_wps_enabled=true \
     persist.radio.RATE_ADAPT_ENABLE=1 \
     persist.radio.ROTATION_ENABLE=1 \
