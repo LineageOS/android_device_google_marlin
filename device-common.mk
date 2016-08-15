7fe6adc473d84a703b07860626fc45aac2a1a658
Dan Stoza <stoza@google.com>
2016-08-15 09:39:57 -0700

    Disable HWC virtual displays
    Disables hardware composer virtual displays, since they were indirectly
    causing screenrecord to jank.
    
    Bug: 30022738
    Change-Id: I6b209c4ba882fbbaf7cdd65ae412d53f1ada6bc9

--- device-common.mk
+++ device-common.mk
@@ -443,3 +443,9 @@ endif
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
 PRODUCT_PROPERTY_OVERRIDES += ro.logd.size=1M
 endif
+
+# b/30022738
+# Work around janky screenrecord performance by disabling hardware composer
+# virtual displays
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.disable_hwc_vds=1
