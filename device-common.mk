ff30dea5ec429f504f45412a93d05ade58bfb787
Prashant Malani <pmalani@google.com>
2016-10-13 14:20:09 -0700

    Add vibrator service for Treble builds
    Bug: 32021191
    Bug: 34252997
    Change-Id: If2412e7abe402fce8b9990308bff1e2ac4224a9b
    Test: Built & booted SF with ENABLE_TREBLE; vibrator works fine.

--- device-common.mk
+++ device-common.mk
@@ -430,7 +430,7 @@ ifeq ($(ENABLE_TREBLE), true)
 PRODUCT_PACKAGES +=                         \
     android.hardware.nfc@1.0-service        \
     android.hardware.gatekeeper@1.0-service \
-
+    android.hardware.vibrator@1.0-service
 endif
 
 # Vibrator
