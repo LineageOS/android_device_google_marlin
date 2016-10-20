11e51cd49a7f193010db192fac02443eed87379a
Alexey Polyudov <apolyudov@google.com>
2016-10-20 10:27:59 -0700

    gatekeeper: add hw-binder-aware gatekeeper HAL
    Change-Id: I97b485343f26bd4f79141e8b47d40f061cb3a2fa
    Signed-off-by: Alexey Polyudov <apolyudov@google.com>

--- device-common.mk
+++ device-common.mk
@@ -185,6 +185,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/google/marlin/spn-conf.xml:system/etc/spn-conf.xml
 
+# new gatekeeper HAL
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl
+
 # Common sensor packages
 TARGET_USES_NANOHUB_SENSORHAL := true
 NANOHUB_SENSORHAL_LID_STATE_ENABLED := true
@@ -372,8 +376,10 @@ PRODUCT_PACKAGES += \
     android.hardware.nfc@1.0-impl \
 
 ifeq ($(ENABLE_TREBLE), true)
-PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.0-service
+PRODUCT_PACKAGES +=                         \
+    android.hardware.nfc@1.0-service        \
+    android.hardware.gatekeeper@1.0-service \
+
 endif
 
 # Vibrator
