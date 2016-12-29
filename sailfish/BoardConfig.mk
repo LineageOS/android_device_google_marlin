adeace35e1e9d15cfb2125f6d90cda16ce71515b
Dan Pasanen <dan.pasanen@gmail.com>
2016-12-29 08:58:05 -0600

    marlin/sailfish: initial lineage bringup
    Change-Id: I33e512ffe3395a8263d22456e2fb37dba9b8bca8

--- sailfish/BoardConfig.mk
+++ sailfish/BoardConfig.mk
@@ -3,6 +3,9 @@
 # Product-specific compile-time definitions
 #
 
+# Allow Lineage config to override others
+-include device/google/marlin/sailfish/BoardConfigLineage.mk
+
 TARGET_BOARD_PLATFORM := msm8996
 TARGET_BOOTLOADER_BOARD_NAME := sailfish
 TARGET_BOARD_INFO_FILE := device/google/marlin/sailfish/board-info.txt
