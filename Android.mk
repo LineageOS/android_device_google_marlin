dcefdd1a8509bea570fd73149f1527ad5be1d5fe
Dan Pasanen <dan.pasanen@gmail.com>
2016-12-29 12:07:33 -0600

    m1s1: Build vendor image and import proprietary-files listings
      Author: Dan Pasanen <dan.pasanen@gmail.com>
      Date:   Thu Dec 29 12:07:33 2016 -0600
          marlin: add proprietary files
          Change-Id: I152db0ae552b865accebfaecc26ccfdc7bade270
    
      Author: Ethan Chen <intervigil@gmail.com>
      Date:   Mon Feb 19 22:35:31 2018 -0800
          marlin: Don't relocate vendor/app platform apps to priv-app
          Change-Id: Ib8dda589bda95d0911768e0f1d4535fcfdeacae8
    
      Author: razorloves <razorloves@gmail.com>
      Date:   Mon Apr 16 19:52:00 2018 -0500
          marlin: Add nexus.xml sysconfig to proprietary files list
          Change-Id: I0dc860b81f706f051c34ea3220d5f3fcf8b173b7
    
      Author: razorloves <razorloves@gmail.com>
      Date:   Mon Oct 9 14:05:53 2017 +0300
          marlin: Add google camera dependencies
          Change-Id: I168c709f038711f9dced46aac813dcdec84b7307
    
      Author: razorloves <razorloves@gmail.com>
      Date:   Thu Aug 2 19:59:52 2018 -0500
          marlin: Add proprietary verizon bits for wfc/vowifi
          Change-Id: I0e235ad2c6184d26f9822f8e355c08bb5be64b66
    
      Author: razorloves <razorloves@gmail.com>
      Date:   Sun Aug 12 22:55:48 2018 -0500
          marlin: Update proprietary files for Pie
          Change-Id: I384f980da5bb89907944eeb672befa1fa8c6431d
    
      Author: razorloves <razorloves@gmail.com>
      Date:   Wed Jun 12 22:16:12 2019 -0500
          marlin: Add libakuaf.so to proprietary files
          Change-Id: Iff52de30535e3ff5675400e77f026be8ecb19db2
    
      Author: razorloves <razorloves@gmail.com>
      Date:   Mon Jan 28 01:37:33 2019 -0600
          marlin: Build the vendor image
          Change-Id: I372eacaae36a3c0e2a7f5e5eff45f0a9186ae427
    
      Author: razorloves <razorloves@gmail.com>
      Date:   Mon Jan 28 06:20:10 2019 -0600
          marlin: Add some missing packages and props
          Change-Id: I2ac172bdd58310cc8c8473545ff64df1a3074bc7
    
      Author: razorloves <razorloves@gmail.com>
      Date:   Thu Feb 28 20:17:12 2019 -0600
          marlin: Setup separate extract scripts for marlin and sailfish
          Change-Id: Ifc608ebea43d0b0326d67c763cb77a9a41967d6e
    
      Author: razorloves <razorloves@gmail.com>
      Date:   Sat Mar 9 00:54:50 2019 -0600
          marlin/sailfish: Update proprietary files
          Change-Id: Ifc131f98803f874be1bede3d2756709bc4b68dc2
    
      Author: razorloves <razorloves@gmail.com>
      Date: Sun, 15 Sep 2019 23:22:30 -0500
          marlin/sailfish: Proprietary file updates for Q
            * Update path for items moved to product/
            * Update for all changes that were done to stock vendor img
          Change-Id: I93f6537647fdecf987d4c439224831345e94dfce
    
      Author: Max Bires <jbires@google.com>
      Date: Tue, 19 Jun 2018 10:05:00 -0700
          marlin/sailfish: Adding VENDOR_SECURITY_PATCH level definition
          * This will fix failing CTS tests for marlin/sailfish.
          Bug: 80210450
          Test: CTS tests pass
          Change-Id: I0d923d4eb67ddef3be81409676472fe520bbacf5
    
      Author: razorloves <razorloves@gmail.com>
      Date: Tue, 17 Sep 2019 00:57:54 -0500
          marlin: Uprev drm hal to 1.2 for Q
          Change-Id: Icf7e66c8af3bf0ff683375fb80674fcfc3bbfa00
    
      Author: "Kevin F. Haggerty" <haggertk@lineageos.org>
      Date: Fri, 9 Apr 2021 08:18:37 -0600
          m1s1: Don't let builds complete without vendor tree
          * There is zero reason to ever build without blobs. We've even seen
            this situation with official builds from our servers. It's always
            better for a build to fail than it is for it to produce something
            that has no chance at working.
          Change-Id: I4968795670c91f691e9ecdc0e4af62e16ba3a93a
    
    Change-Id: I9e28be35733cb51eeac66481e7e798d41c050f04

--- Android.mk
+++ Android.mk
@@ -29,4 +29,24 @@ LOCAL_PATH := $(call my-dir)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
+IMS_LIBS := libimscamera_jni.so libimsmedia_jni.so
+IMS_SYMLINKS := $(addprefix $(TARGET_OUT)/app/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
+$(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "IMS lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /system/lib64/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(IMS_SYMLINKS)
+
+DM_LIBS := libdmengine.so libdmjavaplugin.so
+DM_SYMLINKS := $(addprefix $(TARGET_OUT_PRODUCT)/priv-app/DMService/lib/arm/,$(notdir $(DM_LIBS)))
+$(DM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "DMService lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /system/product/lib/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(DM_SYMLINKS)
+
 endif
