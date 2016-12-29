66b73626c0ba132c9580d6bb09e382d16c22c22e
Dan Pasanen <dan.pasanen@gmail.com>
2016-12-29 08:58:05 -0600

    marlin/sailfish: support inline kernel building
    Change-Id: Id9441f1f971c1ea804b0f4e655ab5013ddcceb08

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -1,3 +1,9 @@
 # Common board config for marlin, sailfish
 
+# Kernel
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+TARGET_KERNEL_CONFIG := lineageos_marlin_defconfig
+TARGET_KERNEL_SOURCE := kernel/google/marlin
+
 -include vendor/google/marlin/BoardConfigVendor.mk
