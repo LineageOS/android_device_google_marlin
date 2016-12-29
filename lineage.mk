06b0eede0ce442d1d4fdc2465eaff8ebb67500f1
Dan Pasanen <dan.pasanen@gmail.com>
2016-12-29 12:07:33 -0600

    marlin: add proprietary files
    * Based off self-extractors/extract-lists.txt
    * marlin-nmf26q
    
    Change-Id: I152db0ae552b865accebfaecc26ccfdc7bade270

--- lineage.mk
+++ lineage.mk
@@ -21,3 +21,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=marlin \
     BUILD_FINGERPRINT="google/marlin/marlin:7.1.1/NMF26Q/3537182:user/release-keys" \
     PRIVATE_BUILD_DESC="marlin-user 7.1.1 NMF26Q 3537182 release-keys"
+
+$(call inherit-product-if-exists, vendor/google/marlin/marlin-vendor.mk)
