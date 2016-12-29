d776556758bbfa58676c89def740f83099879270
Dan Pasanen <dan.pasanen@gmail.com>
2016-12-29 08:58:05 -0600

    marlin: support inline kernel building
    Change-Id: Id9441f1f971c1ea804b0f4e655ab5013ddcceb08

---
+++ marlin/BoardConfigLineage.mk
@@ -0,0 +1,5 @@
+# Kernel
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+TARGET_KERNEL_SOURCE := kernel/google/marlin
+TARGET_KERNEL_CONFIG := lineageos_marlin_defconfig
