34663d8bf6acbf77c179b01f0a0c5e48d8dec254
Dan Pasanen <dan.pasanen@gmail.com>
2016-12-29 18:18:08 -0600

    marlin: build brillo_update_payload
    * For whatever reason this isn't getting built automatically
    
    Change-Id: Ie27c2050a7b14f04dd14f5ce1e6b86950b7d0df8

--- lineage.mk
+++ lineage.mk
@@ -8,6 +8,8 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit device configuration
 $(call inherit-product, device/google/marlin/aosp_marlin.mk)
 
+-include device/google/marlin/device-lineage.mk
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_marlin
 PRODUCT_BRAND := google
