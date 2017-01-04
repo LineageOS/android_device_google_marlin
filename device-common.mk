096d52e4d6facc76ee512715e9fdfda45f8b9b4c
Tao Bao <tbao@google.com>
2017-01-03 23:38:59 -0800

    recovery: Drop dead updater code.
    The vendor-specific updater extensions (BootUpdateFn and DecryptFn) are
    only meaningful for non-A/B updater. They shouldn't be built on A/B
    devices.
    
    This CL removes the updater related functions, and renames the module to
    libgptutils which is used by boot control HAL.
    
    Bug: 33607778
    Test: m checkbuild
    Test: Apply an update on top of the new system.
    Change-Id: Ifad981602a426bd6560a2a3142f49110522f9c88

--- device-common.mk
+++ device-common.mk
@@ -332,7 +332,7 @@ PRODUCT_PACKAGES += \
 # boot_control HAL and its dependencies.
 PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     bootctrl.msm8996 \
-    librecovery_updater_msm \
+    libgptutils \
     libsparse_static
 PRODUCT_PACKAGES += \
     update_engine_sideload
