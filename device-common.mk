535a386a36b85a9ba57e16fecc7530e0150e70b0
Mikhail Naganov <mnaganov@google.com>
2017-01-05 09:23:20 -0800

    Add audio HAL service
    Change-Id: I3f4eb9bc3e4e08c7af361378ed0188fc485c93e8
    Test: reboot device, run "adb shell ps | grep audio"
    Bug: 32022304
    Bug: 30222631

--- device-common.mk
+++ device-common.mk
@@ -130,6 +130,11 @@ PRODUCT_PACKAGES += \
     android.hardware.broadcastradio@1.0-impl \
     android.hardware.soundtrigger@2.0-impl
 
+ifeq ($(ENABLE_TREBLE), true)
+PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-service
+endif
+
 # set audio fluence, ns, aec property
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.qc.sdk.audio.fluencetype=fluencepro \
