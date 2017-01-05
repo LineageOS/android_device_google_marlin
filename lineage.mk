137697b7da059b72cd6d7b139f4aee6b6dd04713
Dan Pasanen <dan.pasanen@gmail.com>
2017-01-05 17:56:53 -0600

    marlin: add lineage makefile
    Change-Id: I33e512ffe3395a8263d22456e2fb37dba9b8bca8

--- lineage.mk
+++ lineage.mk
@@ -2,14 +2,14 @@
 TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
-# Inherit some common CM stuff.
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/marlin/aosp_marlin.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_marlin
+PRODUCT_NAME := lineage_marlin
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel XL
 TARGET_MANUFACTURER := HTC
