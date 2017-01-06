d0c18bea19edbe7c3951eb67169d3836b9f92b5b
Dan Pasanen <dan.pasanen@gmail.com>
2017-01-05 19:27:30 -0600

    marlin: disable bt testing/debugging apps
    Change-Id: I16b98c9a2bf27b1f515eee1c350928dc9cefc5eb

--- common/base.mk
+++ common/base.mk
@@ -669,11 +669,7 @@ PRODUCT_PACKAGES := \
 ifneq ($(TARGET_USES_AOSP),true)
 PRODUCT_PACKAGES += \
        BluetoothExt \
-       BTTestApp \
-       HiddTestApp \
-       BTLogKit \
        a4wpservice \
-       BTLogSave \
        wipowerservice
 endif
 
