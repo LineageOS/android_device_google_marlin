3167fd56a9048abd68fa54cc66a5f4ec569c074e
Ethan Chen <intervigil@gmail.com>
2017-01-08 19:44:54 -0800

    marlin: Compile GPS HAL from hardware/qcom/gps
    Change-Id: I0b587ff930325aafdeb27077eeacde2d119cc6ca

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -6,4 +6,8 @@ TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_CONFIG := lineageos_marlin_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/marlin
 
+# GPS
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
+BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET := true
+
 -include vendor/google/marlin/BoardConfigVendor.mk
