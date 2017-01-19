02d2fd1c318184e6b1e062d9fac5d7064fd08dbb
Janis Danisevskis <jdanis@google.com>
2017-01-19 17:10:03 +0000

    Fixed typo in common device.mk
    This typo prevented the keymaster hal iplementation
    from being build for pixel devices.
    
    Test: lunch marlin-eng; make; fastboot flashall -w
    Bug: 34259073
    Change-Id: I2571b1aaf37d13f90d0da548f19a457043a58b12

--- device-common.mk
+++ device-common.mk
@@ -111,7 +111,7 @@ PRODUCT_PACKAGES += \
 
 # Keymaster HAL
 PRODUCT_PACKAGES += \
-    android.ardware.keymaster@3.0-impl
+    android.hardware.keymaster@3.0-impl
 
 ifeq ($(ENABLE_TREBLE), true)
 PRODUCT_PACKAGES += \
