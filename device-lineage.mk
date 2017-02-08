6cb3abc096aa37234a40c8dfec5a0a0e117b6c71
Dan Pasanen <dan.pasanen@gmail.com>
2017-02-08 16:05:28 -0600

    marlin: enable sdcardfs
    * This appears to be working fine with the recent kernel series
    
    Change-Id: Id0c879c00ff49fb5e207092c85d0da191562a15c

--- device-lineage.mk
+++ device-lineage.mk
@@ -15,6 +15,10 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/google/marlin/overlay-lineage
 
+# SDCardFS
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sys.sdcardfs=true
+
 # Snap
 PRODUCT_PACKAGES += Snap
 
