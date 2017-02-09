ba08aac6fb2c0c4a959d015162ffab01aa23899a
Ashutosh Joshi <ashutoshj@google.com>
2017-02-09 21:45:32 +0000

    Revert "Enable binderized sensor service for Marlin."
    This reverts commit 64185b410496146059befa4bbc51e26a23357710.
    
    Change-Id: I9beb58e8ffe8cd496a8d90f25b8ce92b7b2b15d8
    (cherry picked from commit 1019e50b41245d75f606daee2f5476f74125fa18)

--- device-common.mk
+++ device-common.mk
@@ -119,8 +119,7 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     android.hardware.nfc@1.0-service \
     android.hardware.power@1.0-service \
-    android.hardware.sensors@1.0-service \
-    android.hardware.vibrator@1.0-service
+    android.hardware.vibrator@1.0-service \
 
 # Light HAL
 PRODUCT_PACKAGES += \
