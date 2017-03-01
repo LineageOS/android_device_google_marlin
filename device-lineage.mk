51e8f1ce1fad4021d706cb3357729a983ab0d5d9
Dan Pasanen <dan.pasanen@gmail.com>
2017-03-01 09:10:41 -0600

    marlin: add nexus.xml for nexus/pixel experience
    Change-Id: I040d65bbe82f8a255f30dcebe760d4ced48e71b4

--- device-lineage.mk
+++ device-lineage.mk
@@ -15,6 +15,10 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/google/marlin/overlay-lineage
 
+# Pixel Experience
+PRODUCT_COPY_FILES += \
+  device/google/marlin/nexus.xml:system/etc/sysconfig/nexus.xml
+
 # Snap
 PRODUCT_PACKAGES += Snap
 
