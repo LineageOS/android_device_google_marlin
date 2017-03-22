a8543f9b6d94b76d25d1e8b8bdf6687d02270eb1
Sunny Goyal <sunnygoyal@google.com>
2017-03-22 15:52:10 -0700

    Including WallpaperPicker in aosp build
    Bug: 35724630
    Test: None
    Change-Id: Ie4812708aaea713502f879d08598d86ab95e8426

--- aosp_marlin.mk
+++ aosp_marlin.mk
@@ -37,5 +37,6 @@ $(call inherit-product, device/google/marlin/device-marlin.mk)
 $(call inherit-product-if-exists, vendor/google_devices/marlin/device-vendor-marlin.mk)
 
 PRODUCT_PACKAGES += \
-    Launcher3
+    Launcher3 \
+    WallpaperPicker
 
