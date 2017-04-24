36603418913490429377664543c3fd2dc2b18a30
Vijay Venkatraman <vijaykv@google.com>
2017-04-24 21:45:08 +0000

    Revert "Replacing copy headers with header libraries"
    This reverts commit f7830197e1de4e06c35586ece28e8924e5b2389b.
    
    Change-Id: I41127517a91520c82d85d3c7a1b4a9c2f540b8ae

--- time-services/Android.mk
+++ time-services/Android.mk
@@ -7,8 +7,7 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE:=time-services
+LOCAL_COPY_HEADERS_TO := time-services
+LOCAL_COPY_HEADERS := ./time_genoff.h
 
-LOCAL_EXPORT_C_INCLUDE_DIRS:=$(LOCAL_PATH)
-
-include $(BUILD_HEADER_LIBRARY)
+include $(BUILD_COPY_HEADERS)
