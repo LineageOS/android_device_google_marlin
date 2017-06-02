8fa1ba8e2f57d03e2ba9d1c3642d207884e23edc
Bill Yi <byi@google.com>
2017-06-02 11:29:29 -0700

    Update blobs list for marlin/sailfish
    Change-Id: I75ad3bfcc4de8078a8b96d0cf47a85462c65ca3b

--- self-extractors/qcom/staging/device-partial.mk
+++ self-extractors/qcom/staging/device-partial.mk
@@ -14,21 +14,19 @@
 
 #  blob(s) necessary for Marlin hardware
 PRODUCT_COPY_FILES := \
-    vendor/qcom/marlin/proprietary/halutil:system/bin/halutil:qcom \
     vendor/qcom/marlin/proprietary/pktlogconf:system/bin/pktlogconf:qcom \
     vendor/qcom/marlin/proprietary/ATT_profiles.xml:system/etc/cne/Nexus/ATT/ATT_profiles.xml:qcom \
     vendor/qcom/marlin/proprietary/ROW_profiles.xml:system/etc/cne/Nexus/ROW/ROW_profiles.xml:qcom \
     vendor/qcom/marlin/proprietary/VZW_profiles.xml:system/etc/cne/Nexus/VZW/VZW_profiles.xml:qcom \
     vendor/qcom/marlin/proprietary/com.android.ims.rcsmanager.xml:system/etc/permissions/com.android.ims.rcsmanager.xml:qcom \
     vendor/qcom/marlin/proprietary/com.android.vzwomatrigger.xml:system/etc/permissions/com.android.vzwomatrigger.xml:qcom \
-    vendor/qcom/marlin/proprietary/vzw_sso_permissions.xml:system/etc/permissions/vzw_sso_permissions.xml:qcom \
+    vendor/qcom/marlin/proprietary/vzw_mvs_permissions.xml:system/etc/permissions/vzw_mvs_permissions.xml:qcom \
     vendor/qcom/marlin/proprietary/com.android.ims.rcsmanager.jar:system/framework/com.android.ims.rcsmanager.jar:qcom \
     vendor/qcom/marlin/proprietary/lib64/libaptX_encoder.so:system/lib64/libaptX_encoder.so:qcom \
     vendor/qcom/marlin/proprietary/lib64/libaptXHD_encoder.so:system/lib64/libaptXHD_encoder.so:qcom \
     vendor/qcom/marlin/proprietary/libclcore_neon.bc:system/lib/libclcore_neon.bc:qcom \
     vendor/qcom/marlin/proprietary/lib64/libiperf.so:system/lib64/libiperf.so:qcom \
     vendor/qcom/marlin/proprietary/lib64/libminui.so:system/lib64/libminui.so:qcom \
-    vendor/qcom/marlin/proprietary/lib64/vendor.qti.qcril.am@1.0.so:system/lib64/vendor.qti.qcril.am@1.0.so:qcom \
     vendor/qcom/marlin/proprietary/libaptX_encoder.so:system/lib/libaptX_encoder.so:qcom \
     vendor/qcom/marlin/proprietary/libaptXHD_encoder.so:system/lib/libaptXHD_encoder.so:qcom \
     vendor/qcom/marlin/proprietary/lib64/libbcc.so:system/lib64/libbcc.so:qcom \
