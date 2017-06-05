6b05bd263a1e3b052dcee83a9b37ff2091ee8ebe
Dan Pasanen <dan.pasanen@gmail.com>
2017-06-05 14:13:19 -0500

    marlin: update fingerprint for NJH47B
    Change-Id: Ib7d7bd32bdd1ca83eaf33c25065521012c555996

--- lineage.mk
+++ lineage.mk
@@ -19,7 +19,7 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=marlin \
-    BUILD_FINGERPRINT=google/marlin/marlin:7.1.2/N2G47O/3852959:user/release-keys \
-    PRIVATE_BUILD_DESC="marlin-user 7.1.2 N2G47O 3852959 release-keys"
+    BUILD_FINGERPRINT=google/marlin/marlin:7.1.2/NJH47B/4021576:user/release-keys \
+    PRIVATE_BUILD_DESC="marlin-user 7.1.2 NJH47B 4021576 release-keys"
 
 $(call inherit-product-if-exists, vendor/google/marlin/marlin-vendor.mk)
