f774b75a4b2dd4992a54e01f579e073252ab530a
Mathieu Chartier <mathieuc@google.com>
2017-06-23 19:54:48 -0700

    Delete product specific mini-debug code
    Enabled by default now for system server jars.
    
    Bug: 62862291
    Test: make
    Test: readelf -e $OUT/system/framework/oat/arm64/services.odex | grep gnu_debug
    Change-Id: I6ace1d511c3bbb0157a6f58f629b9bac2bb78f23

--- device-common.mk
+++ device-common.mk
@@ -573,14 +573,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_SUPPORTS_VERITY_FEC := false
 endif
 
-# Add minidebug info to the system server to support diagnosing native crashes.
-ifneq (,$(filter user userdebug, $(TARGET_BUILD_VARIANT)))
-    # System server and some of its services.
-    # Note: we cannot use PRODUCT_SYSTEM_SERVER_JARS, as it has not been expanded at this point.
-    $(call add-product-dex-preopt-module-config,services,--generate-mini-debug-info)
-    $(call add-product-dex-preopt-module-config,wifi-service,--generate-mini-debug-info)
-endif
-
 # b/35633646
 # Statically linked toybox for modprobe in recovery mode
 PRODUCT_PACKAGES += \
