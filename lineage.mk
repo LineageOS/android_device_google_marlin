f63a4cf9dbc7c5a9275adbf93fd376a9c049734b
razorloves <razorloves@gmail.com>
2017-11-14 02:01:39 -0600

    marlin: update build fingerprint & desc to OPR3.170623.013
    Change-Id: I1c7682564cc56b46045cea3f3b2af8de974df880

--- lineage.mk
+++ lineage.mk
@@ -19,7 +19,7 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=marlin \
-    BUILD_FINGERPRINT=google/marlin/marlin:8.0.0/OPR3.170623.008/4294783:user/release-keys \
-    PRIVATE_BUILD_DESC="marlin-user 8.0.0 OPR3.170623.008 4294783 release-keys"
+    BUILD_FINGERPRINT=google/marlin/marlin:8.0.0/OPR3.170623.013/4397526:user/release-keys \
+    PRIVATE_BUILD_DESC="marlin-user 8.0.0 OPR3.170623.013 4397526 release-keys"
 
 $(call inherit-product-if-exists, vendor/google/marlin/marlin-vendor.mk)
