fd71085bfb0ae05c6e2f630d7ca07012769c8792
razorloves <razorloves@gmail.com>
2018-01-01 20:07:41 -0600

    marlin: use built lights hal on /system instead of /vendor
    The battery led function is disabled in the prebuilt lights hal
    on /vendor, so lets add it to /system so that it can be enabled.
    
    Change-Id: I349ba1589d224d8a3dd389e6d26c5948213fce6c

--- sailfish/device-lineage.mk
+++ sailfish/device-lineage.mk
@@ -1,3 +1,10 @@
+# Lights
+PRODUCT_PACKAGES += \
+    lights.sailfish_sys
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.lights=sailfish_sys
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/google/marlin/sailfish/overlay-lineage
 
