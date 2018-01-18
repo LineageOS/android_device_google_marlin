3f5e75f132b9e752159cff3b0fbce386464d0659
Abhisek Devkota <ciwrl@lineageos.org>
2018-01-17 21:53:21 -0800

    marlin: IMS Support
    Build what we can
    
    Change-Id: I9e482b9c35131e3634aaf7a6c8ba05ade11b28fb

--- device-lineage.mk
+++ device-lineage.mk
@@ -1,3 +1,9 @@
+# IMS
+PRODUCT_PACKAGES += \
+    com.android.ims.rcsmanager \
+    RcsService \
+    PresencePolling
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/google/marlin/overlay-lineage
 
