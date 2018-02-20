a7a18686f51ee0c3e469f81ebb5f38416699c870
Ethan Chen <intervigil@gmail.com>
2018-02-19 17:24:28 -0800

    marlin: Signal that we provide our own telephony-ext
    Change-Id: Ie11734333416b1ea132612a759be8d6277d86b04

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -6,4 +6,7 @@ TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_CONFIG := lineageos_marlin_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/marlin
 
+# Telephony
+TARGET_PROVIDES_TELEPHONY_EXT := true
+
 -include vendor/google/marlin/BoardConfigVendor.mk
