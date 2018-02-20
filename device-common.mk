1262a45e7f29b057660dd08019b67b103ec58eea
Ethan Chen <intervigil@gmail.com>
2018-02-19 17:00:26 -0800

    marlin: Remove debug packages from Lineage build
    Change-Id: Ia33ffffdd615ca8e3313bb9416be27f4512db852

--- device-common.mk
+++ device-common.mk
@@ -290,9 +290,11 @@ PRODUCT_PACKAGES += \
 
 # sensor utilities (only for userdebug and eng builds)
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+ifeq (,$(filter lineage_marlin lineage_sailfish, $(TARGET_PRODUCT)))
 PRODUCT_PACKAGES += \
     nanotool \
     sensortest
+endif
 endif
 
 PRODUCT_COPY_FILES += \
@@ -529,10 +531,12 @@ PRODUCT_PACKAGES += \
 
 # Library used for VTS tests  (only for userdebug and eng builds)
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+ifeq (,$(filter lineage_marlin lineage_sailfish, $(TARGET_PRODUCT)))
 # For VTS profiling.
 PRODUCT_PACKAGES += \
      libvts_profiling \
      libvts_multidevice_proto
+endif
 endif
 
 # NFC/camera interaction workaround - DO NOT COPY TO NEW DEVICES
