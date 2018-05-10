68c424bb0284daf4c1850c994a5416944d7d5139
Ethan Chen <intervigil@gmail.com>
2018-05-09 21:07:04 -0700

    marlin: Update fingerprint for May
    Change-Id: Ie51587c58223807601d275ea039011a9a6dc44da

--- lineage.mk
+++ lineage.mk
@@ -19,7 +19,7 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=marlin \
-    BUILD_FINGERPRINT=google/marlin/marlin:8.1.0/OPM2.171019.029/4657601:user/release-keys \
-    PRIVATE_BUILD_DESC="marlin-user 8.1.0 OPM2.171019.029 4657601 release-keys"
+    BUILD_FINGERPRINT=google/marlin/marlin:8.1.0/OPM4.171019.016.B1/4720843:user/release-keys \
+    PRIVATE_BUILD_DESC="marlin-user 8.1.0 OPM4.171019.016.B1 4720843 release-keys"
 
 $(call inherit-product-if-exists, vendor/google/marlin/marlin-vendor.mk)
