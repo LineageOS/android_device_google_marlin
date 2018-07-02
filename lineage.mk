1e8dfa4589ea0046354bc553e982c764605ecb7c
razorloves <razorloves@gmail.com>
2018-07-02 12:55:14 -0500

    marlin: update build fingerprint to July 2018 OPM4.171019.021.P1
    Change-Id: Ibcbd3654979ac66c72284c1b38dde12ba4d2ccd7

--- lineage.mk
+++ lineage.mk
@@ -19,7 +19,7 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=marlin \
-    BUILD_FINGERPRINT=google/marlin/marlin:8.1.0/OPM4.171019.021.D1/4768809:user/release-keys \
-    PRIVATE_BUILD_DESC="marlin-user 8.1.0 OPM4.171019.021.D1 4768809 release-keys"
+    BUILD_FINGERPRINT=google/marlin/marlin:8.1.0/OPM4.171019.021.P1/4820305:user/release-keys \
+    PRIVATE_BUILD_DESC="marlin-user 8.1.0 OPM4.171019.021.P1 4820305 release-keys"
 
 $(call inherit-product-if-exists, vendor/google/marlin/marlin-vendor.mk)
