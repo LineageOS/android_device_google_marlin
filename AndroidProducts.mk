308255caa3ea1c11cdde2a2af35c593ad71cc552
Bruno Martins <bgcngm@gmail.com>
2018-08-10 13:34:48 +0100

    marlin: Consider lineage_marlin product via the AOSP way
    Change-Id: If4e63de0e6e4150f086a8a912a0e9b2ec161492d

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,4 +1,5 @@
 PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/aosp_marlin.mk \
 	$(LOCAL_DIR)/aosp_marlin_svelte.mk \
-	$(LOCAL_DIR)/aosp_sailfish.mk
+	$(LOCAL_DIR)/aosp_sailfish.mk \
+	$(LOCAL_DIR)/lineage_marlin.mk
