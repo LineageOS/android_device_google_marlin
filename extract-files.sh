853fdc758f1eac332eb85988b9ca19adbb24f1e7
razorloves <razorloves@gmail.com>
2018-08-12 22:55:48 -0500

    marlin: Update proprietary files for Pie
    Change-Id: I384f980da5bb89907944eeb672befa1fa8c6431d

--- extract-files.sh
+++ extract-files.sh
@@ -53,4 +53,8 @@ setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
 
 extract "$MY_DIR"/device-proprietary-files.txt "$SRC"
 
+# Don't disable MyVerizonServices app
+sed -i 's|<disabled-until-used-preinstalled-carrier-app package="com.verizon.mips.services" />|<!--disabled-until-used-preinstalled-carrier-app package="com.verizon.mips.services" /-->|g'\
+    "$CM_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/etc/sysconfig/nexus.xml
+
 "$MY_DIR"/setup-makefiles.sh
