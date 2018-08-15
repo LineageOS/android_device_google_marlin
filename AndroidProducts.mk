d92fe8a7d23d6198fd4ec0306b7ae729f9907f36
razorloves <razorloves@gmail.com>
2018-08-14 23:53:31 -0500

    sailfish: Add sailfish lineage mk in marlin repo
    Change-Id: Ifc54834c0e3ee570c6d106b8fb5011a91c19a80d

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,4 +2,5 @@ PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/aosp_marlin.mk \
 	$(LOCAL_DIR)/aosp_marlin_svelte.mk \
 	$(LOCAL_DIR)/aosp_sailfish.mk \
-	$(LOCAL_DIR)/lineage_marlin.mk
+	$(LOCAL_DIR)/lineage_marlin.mk \
+	$(LOCAL_DIR)/lineage_sailfish.mk
