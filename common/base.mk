16194073ab1c4a019f4e6065006fec3205fded77
Ethan Chen <intervigil@gmail.com>
2018-08-18 10:06:03 -0700

    marlin: Don't build mm-qcamera-app
    Change-Id: I6033470c605665ff4338d26e15550b985c5d007e

--- common/base.mk
+++ common/base.mk
@@ -330,7 +330,6 @@ LIBCAMERA += libmmcamera_interface2
 LIBCAMERA += libmmjpeg_interface
 LIBCAMERA += libmmlib2d_interface
 LIBCAMERA += libqomx_core
-LIBCAMERA += mm-qcamera-app
 LIBCAMERA += camera_test
 LIBCAMERA += org.codeaurora.camera
 
