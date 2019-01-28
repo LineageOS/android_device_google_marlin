76f2d45674467e70e2a1b064252bd091dc92d0c0
razorloves <razorloves@gmail.com>
2019-01-28 01:37:33 -0600

    marlin: Build the vendor image
    Change-Id: I372eacaae36a3c0e2a7f5e5eff45f0a9186ae427

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -6,4 +6,8 @@ TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_CONFIG := lineageos_marlin_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/marlin
 
+# Build vendor image
+BOARD_VENDORIMAGE_PARTITION_SIZE := 314572800
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+
 -include vendor/google/marlin/BoardConfigVendor.mk
