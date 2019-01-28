b57eb8b6c36a729fdd1829e464d3616dae701a53
razorloves <razorloves@gmail.com>
2019-01-28 01:37:33 -0600

    marlin/sailfish: Build the vendor image
    This is a squash of the following commits:
    
      Author: razorloves <razorloves@gmail.com>
      Date:   Mon Jan 28 01:37:33 2019 -0600
          marlin: Build the vendor image
          Change-Id: I372eacaae36a3c0e2a7f5e5eff45f0a9186ae427
    
      Author: razorloves <razorloves@gmail.com>
      Date:   Mon Jan 28 06:20:10 2019 -0600
          marlin: Add some missing packages and props
          Change-Id: I2ac172bdd58310cc8c8473545ff64df1a3074bc7
    
      Author: razorloves <razorloves@gmail.com>
      Date:   Thu Feb 28 20:17:12 2019 -0600
          marlin: Setup separate extract scripts for marlin and sailfish
          Change-Id: Ifc608ebea43d0b0326d67c763cb77a9a41967d6e
    
      Author: razorloves <razorloves@gmail.com>
      Date:   Sat Mar 9 00:54:50 2019 -0600
          marlin/sailfish: Update proprietary files
          Change-Id: Ifc131f98803f874be1bede3d2756709bc4b68dc2
    
    Change-Id: I372eacaae36a3c0e2a7f5e5eff45f0a9186ae427

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -1,9 +1,11 @@
 # Common board config for marlin, sailfish
 
+# Build vendor image
+BOARD_VENDORIMAGE_PARTITION_SIZE := 314572800
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+
 # Kernel
 BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_CONFIG := lineageos_marlin_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/marlin
-
--include vendor/google/marlin/BoardConfigVendor.mk
