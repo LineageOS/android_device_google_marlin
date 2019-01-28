faf77078e03bbc245cae0f876dd1c34202862d3c
razorloves <razorloves@gmail.com>
2019-01-28 01:51:44 -0600

    marlin: Squashed reverts of vendor img hacks
    Building vendor image now so these hacks are no longer needed.
    
      Revert "marlin: don't overwrite frameworks-res from vendor"
      This reverts commit bfa6ea530d59eb6da6ab552ec5f45b9947d6140b.
    
      Revert "marlin: Override theme overlay property"
      This reverts commit 6b9a5347acd3ba94cf1265c5d13f5a34abd0a557.
    
      Revert "marlin: use built lights hal on /system instead of /vendor"
      This reverts commit 1e5bbe9c8e390d402d70852221c6a6ffc100ca0f.
    
      Revert "marlin: Add Google platform certificate"
      This reverts commit 26e577be8796a60d1fd2990ba769d46d51d6004c.
    
    Change-Id: I8eb96b8074963cb417a42cd0eae3637a6712f50f

--- sailfish/device-lineage.mk
+++ sailfish/device-lineage.mk
@@ -1,11 +1,1 @@
-# Lights
-PRODUCT_PACKAGES += \
-    lights.sailfish_sys
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hardware.lights=sailfish_sys
-
-# Overlays
-DEVICE_PACKAGE_OVERLAYS += device/google/marlin/sailfish/overlay-lineage
-
 $(call inherit-product, device/google/marlin/device-lineage.mk)
