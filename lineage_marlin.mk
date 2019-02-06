35a7e19540fe97cb71d0a5d9a368e4448b8c2d6c
razorloves <razorloves@gmail.com>
2019-02-06 06:53:02 -0600

    marlin: Update build fingerprint to Feb 2019 PQ2A.190205.003
    Change-Id: I23303cdf3cadb652f164667a47e7180c3d5db521

--- lineage_marlin.mk
+++ lineage_marlin.mk
@@ -19,8 +19,8 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=marlin \
-    PRIVATE_BUILD_DESC="marlin-user 9 PQ1A.190105.004 5148680 release-keys"
+    PRIVATE_BUILD_DESC="marlin-user 9 PQ2A.190205.003 5180536 release-keys"
 
-BUILD_FINGERPRINT := google/marlin/marlin:9/PQ1A.190105.004/5148680:user/release-keys
+BUILD_FINGERPRINT := google/marlin/marlin:9/PQ2A.190205.003/5180536:user/release-keys
 
 $(call inherit-product-if-exists, vendor/google/marlin/marlin-vendor.mk)
--- lineage_sailfish.mk
+++ lineage_sailfish.mk
@@ -19,8 +19,8 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=sailfish \
-    PRIVATE_BUILD_DESC="sailfish-user 9 PQ1A.190105.004 5148680 release-keys"
+    PRIVATE_BUILD_DESC="sailfish-user 9 PQ2A.190205.003 5180536 release-keys"
 
-BUILD_FINGERPRINT := google/sailfish/sailfish:9/PQ1A.190105.004/5148680:user/release-keys
+BUILD_FINGERPRINT := google/sailfish/sailfish:9/PQ2A.190205.003/5180536:user/release-keys
 
 $(call inherit-product-if-exists, vendor/google/marlin/marlin-vendor.mk)
