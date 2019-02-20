94e7b30686438b48b2242d3e14d6a5b932c98b0a
Sean Callanan <spyffe@google.com>
2019-02-20 12:45:08 -0800

    marlin: switch to IMapper 2.1
    Test: display CTS
    Bug: 62535446
    Change-Id: I2d6d0d118014eb2b99a6832b4c900ae21c062517

--- device-common.mk
+++ device-common.mk
@@ -511,7 +511,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl:64 \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.mapper@2.0-impl
+    android.hardware.graphics.mapper@2.0-impl-2.1
 
 # HW Composer
 PRODUCT_PACKAGES += \
