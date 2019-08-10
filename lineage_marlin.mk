a891e5d87aaca22022695a8b371ab8ac9965d5ea
razorloves <razorloves@gmail.com>
2019-08-10 16:47:15 -0500

    marlin/sailfish: Update build fingerprint to Aug 2019 PQ3A.190801.002
    Change-Id: I983ebe5f4dec1db325808e5a0774778ddb93843b

--- lineage_marlin.mk
+++ lineage_marlin.mk
@@ -19,8 +19,8 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=marlin \
-    PRIVATE_BUILD_DESC="marlin-user 9 PQ3A.190705.001 5565753 release-keys"
+    PRIVATE_BUILD_DESC="marlin-user 9 PQ3A.190801.002 5670241 release-keys"
 
-BUILD_FINGERPRINT := google/marlin/marlin:9/PQ3A.190705.001/5565753:user/release-keys
+BUILD_FINGERPRINT := google/marlin/marlin:9/PQ3A.190801.002/5670241:user/release-keys
 
 $(call inherit-product-if-exists, vendor/google/marlin/marlin-vendor.mk)
