4fd24c6dc7206a25811dd4991f555ec585ede6e1
Nolen Johnson <johnsonnolen@gmail.com>
2022-02-23 13:54:26 -0500

    m1s1: Organize recovery files
    Change-Id: I859534db37c1ddcb6abba5b62e4ef9a559411516

--- device-sailfish.mk
+++ device-sailfish.mk
@@ -46,7 +46,7 @@ PRODUCT_COPY_FILES += \
     device/google/marlin/fstab.common:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.sailfish \
     device/google/marlin/fstab.common:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.sailfish \
     device/google/marlin/audio/audio_platform_info_tasha_sailfish.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_tasha.xml \
-    device/google/marlin/init.recovery.common.rc:recovery/root/init.recovery.sailfish.rc
+    device/google/marlin/recovery/init.recovery.common.rc:recovery/root/init.recovery.sailfish.rc
 
 # Sensor packages
 PRODUCT_PACKAGES += \
