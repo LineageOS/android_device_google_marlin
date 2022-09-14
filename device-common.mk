d67919f11fbc0ae935d327b974486e4d3b52b757
Bruno Martins <bgcngm@gmail.com>
2022-09-14 14:51:15 +0100

    m1s1: Migrate to AIDL ClearKey DRM HAL
    Change-Id: Iad5cc1176978137c647069197b33f49fb4d98ab2

--- device-common.mk
+++ device-common.mk
@@ -632,7 +632,7 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm-service.clearkey
 
 PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true \
