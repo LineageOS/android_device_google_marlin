5012922fd3ac4c06984a6e98851d4eeb8333ad15
Nolen Johnson <johnsonnolen@gmail.com>
2023-09-03 00:51:15 -0400

    m1s1: Switch to QCOM sepolicy-legacy-um
    * m1s1 sepolicy was never good, was janked up hard when VNDK-SP
      was introduced and property isolation was never done properly.
    * QCOM sepolicy-legacy-um is much more up to date and accurate.
    * I went through all *_contexts files from QCOM policy & m1s1, then
      removed duplicates from m1s1, updated labels which changed, and
      left the m1s1 specific ones here.
    * Went through and removed conflicting type declarations and updated
      ones which had their names updated.
    * Imported pixel thermal device.mk contents, and relevant sepolicy
      snippets to prevent conflicting genfscon labels with QCOm policy.
    * Cleaned up now duplicated policies (WIP).
    
    Change-Id: I19a0dd74e0abd1dec561279d1a23eccdaa9fc8a4
    (cherry picked from commit 64cb97b441616a149addec992f5a205a898b6291)

--- device-common.mk
+++ device-common.mk
@@ -455,7 +455,24 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.thermal.config=thermal_info_config.json
 
-include hardware/google/pixel/thermal/device.mk
+# Thermal HAL
+SOONG_CONFIG_NAMESPACES += thermal_hal_feature
+SOONG_CONFIG_thermal_hal_feature += \
+    pid \
+
+SOONG_CONFIG_thermal_hal_feature_pid ?= apply_1_0
+
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.pixel
+
+# Thermal utils
+PRODUCT_PACKAGES += \
+    thermal_symlinks
+
+ifneq (,$(filter eng, $(TARGET_BUILD_VARIANT)))
+PRODUCT_PACKAGES += \
+    thermal_logd
+endif
 
 # VR
 PRODUCT_PACKAGES += \
