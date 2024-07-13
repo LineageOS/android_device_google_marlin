3c08a19facaec93c4c9e23368b96e9bc4229e618
Yumi Yukimura <me.cafebabe@gmail.com>
2024-07-13 23:22:02 +0530

    m1s1: Switch to QTI Memtrack AIDL HAL
    Change-Id: Ibcbaff02fd56fbccc3a88c01a5f46a89cb212ef5

--- device-common.mk
+++ device-common.mk
@@ -303,9 +303,7 @@ PRODUCT_COPY_FILES += \
 
 # Memtrack
 PRODUCT_PACKAGES += \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
-    memtrack.msm8996
+    vendor.qti.hardware.memtrack-service
 
 # NFC
 PRODUCT_PACKAGES += \
