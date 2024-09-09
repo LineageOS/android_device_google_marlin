b69f44fd3d0606f0374f94d4e50d3d2f5a69efbd
Michael Bestas <mkbestas@lineageos.org>
2024-09-10 02:07:58 +0300

    m1s1: extract-files improvements
    Change-Id: I517698ca3b5a93e10a5b167417631c7f8d0ab9e2

--- sailfish/setup-makefiles.sh
+++ sailfish/setup-makefiles.sh
@@ -30,7 +30,7 @@ setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 # Warning headers and guards
 write_headers
 
-write_makefiles "${MY_DIR}/device-proprietary-files.txt" true
+write_makefiles "${MY_DIR}/device-proprietary-files.txt"
 write_makefiles "${MY_DIR}/device-proprietary-files-vendor.txt" true
 
 append_firmware_calls_to_makefiles "${MY_DIR}/proprietary-firmware.txt"
