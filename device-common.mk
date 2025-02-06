98aca19057bceea32a0663eb0ce23d2c42f774bb
Michael Bestas <mkbestas@lineageos.org>
2025-02-07 00:36:31 +0200

    m1s1: Don't use BSG framework for gpt-utils
    Change-Id: Ia6e5d651560c8ae5186a48bfbb0e30b864150564

--- device-common.mk
+++ device-common.mk
@@ -100,6 +100,8 @@ PRODUCT_PACKAGES += \
     libgptutils.marlin \
     libgptutils.marlin.recovery
 
+$(call soong_config_set,QTI_GPT_UTILS,USE_BSG_FRAMEWORK,false)
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
