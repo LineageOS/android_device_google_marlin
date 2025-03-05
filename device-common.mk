f48214f2ce646d83e2cfecb3feaaa6f8daa4c821
Nolen Johnson <johnsonnolen@gmail.com>
2025-03-05 16:59:16 -0500

    m1s1: Drop prebuilt kernel support
    * Not like this would have a chance in hell to work.
    
    Change-Id: Ia284b852cc854b85256843cf58fdd14a5e844f80

--- device-common.mk
+++ device-common.mk
@@ -18,12 +18,6 @@
 #
 # Everything in this directory will become public
 
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-    LOCAL_KERNEL := device/google/marlin-kernel/Image.lz4-dtb
-else
-LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
 PRODUCT_SHIPPING_API_LEVEL := 25
 
 PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
