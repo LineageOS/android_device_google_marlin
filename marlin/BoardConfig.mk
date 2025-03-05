2142fbd5535cf804fb95016458dabf6d8a960d67
Nolen Johnson <johnsonnolen@gmail.com>
2025-03-05 16:59:16 -0500

    m1s1: Drop prebuilt kernel support
    * Not like this would have a chance in hell to work.
    
    Change-Id: Ia284b852cc854b85256843cf58fdd14a5e844f80

--- marlin/BoardConfig.mk
+++ marlin/BoardConfig.mk
@@ -28,7 +28,6 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := kryo
 
 TARGET_NO_BOOTLOADER := true
-TARGET_NO_KERNEL := false
 TARGET_NO_RECOVERY := true
 TARGET_RECOVERY_FSTAB := device/google/marlin/init-files/fstab.common
 BOARD_USES_RECOVERY_AS_BOOT := true
