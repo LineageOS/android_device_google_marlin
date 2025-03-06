20a9961c0453df8d7b3afe739b7345cd9b005437
Nolen Johnson <johnsonnolen@gmail.com>
2025-03-06 01:56:51 -0500

    m1s1: Fold device specific makefiles into one makefile
    * The only thing we couldn't just use build variables for
      was AAPT and boot animation resolution, so just if those.
    
    Change-Id: I5049603770f42701989c2f396cc0d3c73e11146f

--- lineage_sailfish.mk
+++ lineage_sailfish.mk
@@ -6,7 +6,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/google/marlin/device-sailfish.mk)
+$(call inherit-product, device/google/marlin/device-common.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_sailfish
