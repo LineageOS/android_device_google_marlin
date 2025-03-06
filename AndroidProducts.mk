c3d601cf01d1ad27a996f6ea06fd9e42955c0174
Nolen Johnson <johnsonnolen@gmail.com>
2025-03-05 23:53:50 -0500

    m1s1: Kill AOSP target support
    * Long dead, we rely on so much Lineage stuff.
    
    Change-Id: I4b69f34fce690f61db2292415125884610b98153

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,9 +1,3 @@
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/aosp_marlin.mk \
-	$(LOCAL_DIR)/aosp_sailfish.mk \
-	$(LOCAL_DIR)/lineage_marlin.mk \
-	$(LOCAL_DIR)/lineage_sailfish.mk
-
-COMMON_LUNCH_CHOICES := \
-	aosp_marlin-userdebug \
-	aosp_sailfish-userdebug
+    $(LOCAL_DIR)/lineage_marlin.mk \
+    $(LOCAL_DIR)/lineage_sailfish.mk
