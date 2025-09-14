a273ba064a38ae8df9c4429d8c5b94b8493a0d98
Michael Bestas <mkbestas@lineageos.org>
2025-09-14 16:46:20 +0300

    m1s1: Migrade audio HAL to blueprint
    Change-Id: I589683d4f2f97b6dbc52fcaa1055a23b0cab8b06

--- device-common.mk
+++ device-common.mk
@@ -57,6 +57,10 @@ PRODUCT_PACKAGES += \
     libqcompostprocbundle \
     libvolumelistener
 
+$(call soong_config_set,qtiaudio,feature_hwdep_cal,true)
+$(call soong_config_set,qtiaudio,feature_multi_voice_sessions,true)
+$(call soong_config_set,qtiaudio,feature_snd_monitor,true)
+
 PRODUCT_COPY_FILES += \
     device/google/marlin/audio/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
     device/google/marlin/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
@@ -393,8 +397,9 @@ PRODUCT_SOONG_NAMESPACES += \
     device/google/marlin \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/qcom/media/msm8996 \
+    hardware/qcom/audio \
     hardware/qcom/display/msm8996 \
+    hardware/qcom/media/msm8996 \
     hardware/qcom/wlan/legacy \
     hardware/qcom-caf/bootctrl
 
